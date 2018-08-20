a0ee4c9da81149e5f719040fa69fd1e4135ad85c
Snoopy <snoopy@MacBook-Pro-de-SERGENT.local>
2018-08-20 11:33:09 +0200

    last commit

--- Delai/ViewController.swift
+++ Delai/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tortue: UILabel!
+    @IBOutlet weak var lievre: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 4, delay: 0, options: .curveEaseIn, animations: {
+            self.tortue.center.x = 30
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 4, delay: 0, options: .curveEaseOut, animations: {
+            self.lievre.center.x = 30
+        }, completion: nil)
     }
-
 
 }
 
